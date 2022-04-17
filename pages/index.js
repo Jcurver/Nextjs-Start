60a5fde512d04e35e896caa75a70d6c18a7b6892
박재현 <eric7213@naver.com>
2022-04-18 00:58:00 +0900

    [ADD] styles 2.1

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
 
-import NavBar from "../components/NavBar";
+const API_KEY = "fdfbf9600a53fd2d0b9c20aee1f37110";
 
 export default function Home() {
-
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
-      <NavBar />
-      <h1>Hello</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
