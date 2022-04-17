a4390abdf0c1a82bea945ca3be40c4964fb99c8d
박재현 <eric7213@naver.com>
2022-04-17 22:36:17 +0900

    [ADD] all next file

---
+++ components/NavBar.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <nav>
+      <Link href="/">
+        <a style={{ color: router.pathname === "/" ? "blue" : "red" }}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === "/about" ? "blue" : "red" }}>
+          About
+        </a>
+      </Link>
+    </nav>
+  );
+}
