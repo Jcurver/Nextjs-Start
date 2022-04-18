7370b5adffea4beb2a8b1fb2ba89580891fdb704
박재현 <eric7213@naver.com>
2022-04-19 01:12:13 +0900

    [ADD] styles 2.7

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const API_KEY = process.env.API_KEY;
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id", // id로 명명해줬으면 destination도 id로 동일하게 명명
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
